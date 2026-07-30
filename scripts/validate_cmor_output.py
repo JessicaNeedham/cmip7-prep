ff76ab99e80bcac1bcc4b78920a03b6b4b28219e
Jessica Needham <jessica@login1-nird-lmd.nird.login.svc.cluster.local>
2026-07-30 13:28:33 +0200

    picontrol back to piControl

--- scripts/validate_cmor_output.py
+++ scripts/validate_cmor_output.py
@@ -275,7 +275,7 @@ def get_requested_variables(
         operation="all",
         cmip7_frequency=frequency,
         modelling_realm=realm,
-        experiment=experiment.lower(),
+        experiment=experiment,
     )
     return {var.branded_variable_name.name for var in cmip_vars}
 
